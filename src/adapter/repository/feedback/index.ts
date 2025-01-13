2b76b25e3ee07612a892b5d1c1d81d49adb8a780
el <shipagakerimov@mail.ru>
2025-01-13 22:00:32 +0500

    upd: inject methods

--- src/adapter/repository/feedback/index.ts
+++ src/adapter/repository/feedback/index.ts
@@ -4,6 +4,7 @@ import { buildUpdate, Update } from './update';
 import { List, buildList } from './list';
 import { buildCreate, Create} from './create'
 import { buildGet, Get} from './get'
+import {buildCount, Count} from '@/adapter/repository/feedback/count';
 
 type Params = Pick<AdapterParams, 'db'>
 
@@ -13,6 +14,7 @@ export type FeedbackRepository = {
     get: Get,
     list: List,
     update: Update,
+    count: Count,
 }
 export const buildFeedbackRepository = (params: Params): FeedbackRepository=>{
   const create = buildCreate(params)
@@ -20,6 +22,7 @@ export const buildFeedbackRepository = (params: Params): FeedbackRepository=>{
   const get = buildGet(params)
   const list = buildList(params)
   const update = buildUpdate(params)
+  const count = buildCount(params)
 
   return {
     create,
@@ -27,5 +30,6 @@ export const buildFeedbackRepository = (params: Params): FeedbackRepository=>{
     get,
     list,
     update,
+    count
   }
 }
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,1 +1,3 @@
 export * from './hello';
+export * from './count-total-pages';
+export * from './offset-for-pagination';
